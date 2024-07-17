5c5e9e05bdcbe72872ecb76f0c3f6f1b1a129fbf
JeralSandeeptha <jeral.sandeeptha1@gmail.com>
2024-07-17 14:47:58 +0530

    FE-6 - Design 404 Page

--- src/pages/notfound-page/NotFoundPage.tsx
+++ src/pages/notfound-page/NotFoundPage.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import './NotFoundPage.scss';
+import Lottie from 'lottie-react';
+import { Link } from 'react-router-dom';
+import animationData from '../../assets/lotties/not-found.json';
 
 const NotFoundPage = () => {
+
   return (
-    <div>NotFoundPage</div>
-  )
+    <div className='notfound'>
+      <div className="notfound-inner">
+        <Lottie animationData={animationData} className='anim'/>
+        <Link to='/' className='link'>
+          <h5 className='notfound-text'>Back to Home</h5>
+        </Link>
+      </div>
+    </div>
+  );
+
 }
 
-export default NotFoundPage
+export default NotFoundPage;
