faef574695312d85dc10a005ad10c21ca889aa75
JeralSandeeptha <jeral.sandeeptha1@gmail.com>
2024-07-21 22:12:34 +0530

    FE-19 Create Home page features section

--- src/pages/landing-page/LandingPage.tsx
+++ src/pages/landing-page/LandingPage.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './LandingPage.scss';
 import HomeHero from '../../components/home-hero/HomeHero';
+import Features from '../../components/features/Features';
 
 const LandingPage = () => {
   return (
     <>
       <HomeHero />
+      <Features />
     </>
   )
 }
