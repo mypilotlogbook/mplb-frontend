00d7e491b9113f85690442cec34411c27413e085
JeralSandeeptha <jeral.sandeeptha1@gmail.com>
2024-08-01 21:59:50 +0530

    FE-28 Create contact page header section

---
+++ src/components/contact-header/ContactHeader.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ContactHeader.scss';
+
+const ContactHeader = () => {
+
+    return (
+        <div className='test contact-header'></div>
+    );
+
+}
+
+export default ContactHeader;
