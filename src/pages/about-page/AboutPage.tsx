0ddfce5a24f9518b90ef4bc26d741eb828117ab6
JeralSandeeptha <jeral.sandeeptha1@gmail.com>
2024-11-02 00:04:08 +0530

    Add react-helmet and SEO home, about and contact pages

--- src/pages/about-page/AboutPage.tsx
+++ src/pages/about-page/AboutPage.tsx
@@ -4,11 +4,27 @@ import AboutHero from '../../components/about-hero/AboutHero';
 import AboutSection from '../../components/about-section/AboutSection';
 import FooterTop from '../../components/footer-top/FooterTop';
 import Footer from '../../components/footer/Footer';
+import { Helmet } from 'react-helmet';
 
 const AboutPage = () => {
 
   return (
     <>
+      <Helmet>
+
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <title>About My Pilot Logbook | Your Trusted Digital Flight Logging Partner</title>
+        <meta name="description" content="Learn more about My Pilot Logbook, our mission to support pilots with an easy-to-use digital logbook, and our commitment to helping you streamline your flight records with ease." />
+        <meta name="keywords" content="about My Pilot Logbook, digital pilot logbook, flight logging solutions, aviation record keeping, pilot flight management, logbook for pilots" />
+
+        <meta property="og:title" content="About My Pilot Logbook | Your Trusted Digital Flight Logging Partner" />
+        <meta property="og:description" content="Learn more about My Pilot Logbook, our mission to support pilots with an easy-to-use digital logbook, and our commitment to helping you streamline your flight records with ease." />
+        <meta property="og:image" content="https://res.cloudinary.com/dv9ax00l4/image/upload/v1723141068/logo-mplb_urrcoa.png" />
+        <meta property="og:type" content="website" />
+
+      </Helmet>
+
       <AboutHero />
       <AboutSection />
       <FooterTop />
--- src/pages/contact-page/ContactPage.tsx
+++ src/pages/contact-page/ContactPage.tsx
@@ -4,10 +4,26 @@ import ContactHero from '../../components/contact-hero/ContactHero';
 import Footer from '../../components/footer/Footer';
 import ContactHeader from '../../components/contact-header/ContactHeader';
 import ContactForm from '../../components/contact-form/ContactForm';
+import { Helmet } from 'react-helmet';
 
 const ContactPage = () => {
   return (
     <>
+      <Helmet>
+
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <title>Contact Us | My Pilot Logbook - Support and Inquiries</title>
+        <meta name="description" content="Have questions about My Pilot Logbook? Reach out to us for support, feedback, or general inquiries. We're here to help you make the most of our digital logbook for pilots." />
+        <meta name="keywords" content="contact My Pilot Logbook, logbook support, aviation logbook inquiries, pilot logbook questions, pilot support, My Pilot Logbook help" />
+
+        <meta property="og:title" content="Contact Us | My Pilot Logbook - Support and Inquiries" />
+        <meta property="og:description" content="Have questions about My Pilot Logbook? Reach out to us for support, feedback, or general inquiries. We're here to help you make the most of our digital logbook for pilots." />
+        <meta property="og:image" content="https://res.cloudinary.com/dv9ax00l4/image/upload/v1723141068/logo-mplb_urrcoa.png" />
+        <meta property="og:type" content="website" />
+
+      </Helmet>
+      
       <ContactHero />
       <ContactHeader />
       <ContactForm />
